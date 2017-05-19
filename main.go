48025049db95438554d6867be49694907c45baa0
Yurizal Susanto <rizal.sagi@gmail.com>
2017-05-19 22:19:52 +0700

    Change main for basic use
    Implement graceful shutdown based on time out.

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"github.com/yursan9/tulis/pkg/post"
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/yursan9/tulis/pkg/server"
 )
 
 func main() {
-	p := post.GetPosts("posts")
-	fmt.Println(p)
+	stop := make(chan os.Signal)
+	signal.Notify(stop, os.Interrupt)
+	opt := &server.Options{}
+
+	app := server.New(opt)
+	go func() {
+		log.Println("Listening to port http://127.0.0.1" + app.Addr)
+		log.Fatal(app.ListenAndServe())
+	}()
+	<-stop
+
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	log.Println("Shutting down the server...")
+	app.Shutdown(ctx)
+	log.Println("Server stopped.")
 }
