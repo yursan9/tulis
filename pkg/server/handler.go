659004648448ea76832bb81ca1094e4849ac2a68
Yurizal Susanto <rizal.sagi@gmail.com>
2017-06-01 21:23:53 +0700

    Don't serve NotFound when tag not found

--- pkg/server/handler.go
+++ pkg/server/handler.go
@@ -60,12 +60,9 @@ func About(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 func ByTag(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	p, err := strconv.ParseUint(ps.ByName("page"), 10, 8)
 	if err != nil {
-		http.NotFound(w, r)
+		p = 1
 	}
 	data := newByTagData(uint8(p), ps.ByName("name"))
-	if err != nil {
-		http.NotFound(w, r)
-	}
 	err = t.ExecuteTemplate(w, "tag", data)
 	if err != nil {
 		log.Fatal(err)
